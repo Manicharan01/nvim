eda8fc917f6c5008948986a23fb0733d4026aa60
Manicharan <manicharan150@gmail.com>
2024-08-24 23:15:25 +0530

    Feat: No more italics

--- lua/charan/lazy/arrow.lua
+++ lua/charan/lazy/arrow.lua
@@ -1,11 +1,13 @@
 return {
-    "otavioschwanck/arrow.nvim",
-    config = function()
-        require("arrow").setup({})
-        local persist = require("arrow.persist")
+	"otavioschwanck/arrow.nvim",
+	config = function()
+		require("arrow").setup({
+			show_icons = true,
+		})
+		local persist = require("arrow.persist")
 
-        vim.keymap.set("n", "<leader>l", persist.toggle)
-        vim.keymap.set("n", "<C-h>", persist.next)
-        vim.keymap.set("n", "<C-l>", persist.previous)
-    end,
+		vim.keymap.set("n", "<leader>l", persist.toggle)
+		vim.keymap.set("n", "<C-h>", persist.next)
+		vim.keymap.set("n", "<C-l>", persist.previous)
+	end,
 }
