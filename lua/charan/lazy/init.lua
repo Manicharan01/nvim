454ece9f4830c3de709895091ac9a6e367990de4
Manicharan01 <manicharan150@gmail.com>
2024-09-12 07:37:25 +0530

    disabled Codeium

--- lua/charan/lazy/init.lua
+++ lua/charan/lazy/init.lua
@@ -1,18 +1,18 @@
 return {
-	"eandrju/cellular-automaton.nvim",
-	"nvim-tree/nvim-web-devicons",
-	"echasnovski/mini.icons",
-	"folke/which-key.nvim",
-	"kyazdani42/nvim-web-devicons",
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-	},
-	{
-		"mbbill/undotree",
+    "eandrju/cellular-automaton.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "echasnovski/mini.icons",
+    "folke/which-key.nvim",
+    "kyazdani42/nvim-web-devicons",
+    --{
+    --	"Exafunction/codeium.vim",
+    --	event = "BufEnter",
+    --},
+    {
+        "mbbill/undotree",
 
-		config = function()
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-		end,
-	},
+        config = function()
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        end,
+    },
 }
