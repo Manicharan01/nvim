820826cbc98fb713db0c143da5e972537ca83065
Manicharan <manicharan150@gmail.com>
2025-01-29 18:06:34 +0530

    Trying diffview

--- lua/charan/lazy/colorscheme.lua
+++ lua/charan/lazy/colorscheme.lua
@@ -13,8 +13,8 @@ return {
             require("tokyonight").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
+                style = "night",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true,     -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
@@ -23,7 +23,7 @@ return {
                     keywords = { italic = false },
                     -- Background styles. Can be "dark", "transparent" or "normal"
                     sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
+                    floats = "dark",   -- style for floating windows
                 },
             })
         end,
@@ -40,8 +40,24 @@ return {
 
             vim.cmd("colorscheme rose-pine")
 
-            ColorMyPencils()
+            ColorMyPencils("tokyonight-night")
         end,
     },
-}
 
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha"
+            })
+        end,
+    },
+    {
+        "dupeiran001/nord.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("nord").setup({})
+        end,
+    }
+}
