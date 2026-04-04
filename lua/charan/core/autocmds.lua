4cbb6ba574f1b6b6db5c8c430ea01a956864d865
Manicharan <charan0143@proton.me>
2026-04-04 09:25:26 +0530

    Removed duplicates

--- lua/charan/core/autocmds.lua
+++ lua/charan/core/autocmds.lua
@@ -4,16 +4,6 @@ local autocmd = vim.api.nvim_create_autocmd
 local CharanGroup = augroup("Charan", {})
 local yank_group = augroup("HighlightYank", {})
 
--- Reload helper
-function R(name)
-    return require("plenary.reload").reload_module(name)
-end
-
--- Filetypes
-vim.filetype.add({
-    extension = { templ = "templ" },
-})
-
 -- Format on save
 autocmd("BufWritePre", {
     pattern = "*",
@@ -42,13 +32,6 @@ autocmd({ "BufWritePre" }, {
     group = CharanGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
-})
-
-autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        vim.lsp.buf.format()
-    end
 })
 
 autocmd("LspAttach", {
